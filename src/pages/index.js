922dcc441a0036a96c288dd525f7d895b11ef0b1
Donta Ruffin <donta.ruffin@gmail.com>
2020-11-18 14:21:56 -0500

    Styling update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import "../components/index.css"
 
 export default function Home() {
   return <Layout>
+                <div id={"Home"}>
                 <p>Land Kit leverages the power of computation and algorithmic design for landscape.</p>
                 <p>This compilation of digital design kits provides tools and templates you need to be a more efficient landscape designer and to work more fluidly in 3D.</p>
             
@@ -23,6 +24,6 @@ export default function Home() {
                 <h2>Topo kit</h2>
                 <image></image>
                 <p>Paving Kit is a tool set for making paving design easier. By defining areas, patterns, and color rules, Paving Kit will help with designing, drafting, and modeling your paving for CAD or rendering. </p> 
-              
+                </div>
         </Layout>
         }
